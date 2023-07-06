657c3a13edac9f168b08a11cfca7a2dc1ebbc984
Daniel Castro <daniel.castropineda@gmail.com>
2023-07-06 12:38:20 -0600

    Correcciones tarea 4

--- doc/tarea4/sphere.h
+++ doc/tarea4/sphere.h
@@ -1,3 +1,6 @@
+#ifndef SPHEREH
+#define SPHEREH
+
 #include "vec.h"
 #include "ray.h"
 #pragma once
@@ -18,66 +21,37 @@ public:
     {
         // se asignan los valores de la formula general cuadratica "la del chicharronero"
 
-        // Vector oc = ray.o - p;
-        // double a = ray.d.dot(ray.d);
-        // double b = oc.dot(ray.d);
-        // double c = oc.dot(oc) - r * r;
-        // double discriminant = b * b - a * c;
+        Vector oc = ray.o - p;
+        Vector a = ray.d;
+        double t = 0.0;
+        double tol = 0.00001;
+        double b = oc.dot(ray.d);
+        double c = oc.dot(oc) - r * r;
+        double discriminant = b * b - c;
         // // solo si el valor de discriminant es positivo hacemos la raiz cuadrada para ahorrar tiempo de computo
         // // de otra forma retorna -1
-        // if (discriminant < 0)
-        // {
-        //     return 0.0;
-        // }
-        // else
-        // {
-        //     double tplus = (-b + sqrt(discriminant)) / a;
-        //     double tminus = (-b - sqrt(discriminant)) / a;
-        //     double t;
-
-        //     // ambos positivos
-        //     if (tminus > 0 && tplus > 0)
-        //     {
-        //         t = std::min(tminus, tplus);
-        //     }
-        //     // tminus positivo, tplus negativo
-        //     else if (tminus > 0 && tplus < 0)
-        //     {
-        //         t = tminus;
-        //     }
-        //     // tminus negativo, tplus positivo
-        //     else if (tminus < 0 && tplus > 0)
-        //     {
-        //         t = tplus;
-        //     }
-        //     else
-        //     {
-        //         t = 0;
-        //     }
-        //     return t;
-        // }
-
-        // regresar distancia si hay intersección
-        // regresar 0.0 si no hay interseccion
-        Vector op = ray.o - p;
-        Vector d = ray.d;
-        double t;
-        double tol = 0.00001;
-        double b = op.dot(d);
-        double ce = op.dot(op) - r * r;
-        double disc = b * b - ce;
-        if (disc < 0)
+        if (discriminant < tol)
+        {
             return 0.0;
+        }
         else
-            disc = sqrt(disc);
-        t = -b - disc;
+        {
+            discriminant = sqrt(discriminant);
+        }
+        // tminus
+        t = -b - discriminant;
+
+        // asegurarnos que t sea positivo
         if (t > tol)
             return t;
         else
-            t = -b + disc;
+            // tplus
+            t = -b + discriminant;
+        // asegurarnos que t sea positivo
         if (t > tol)
             return t;
         else
-            return 0;
+            return 0.0;
     }
 };
+#endif
